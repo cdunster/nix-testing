03d8dde5afb7cef70b8431e95b1e8b4d6a584552
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-08 13:37:18 +0100

    chore: add simple Rust bin with devShell support

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { ... }@inputs: {
@@ -13,10 +17,18 @@
     };
   } // inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      pkgs = inputs.nixpkgs.legacyPackages.${system}.appendOverlays [
+        inputs.rust-overlay.overlays.default
+      ];
+      rustFromFile = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
     in
     {
       devShells.default = pkgs.mkShell {
+        packages = [
+          rustFromFile
+        ];
+
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         QEMU_NET_OPTS = "hostfwd=tcp::2221-:22"; # Forward VM SSH port to host port 2221
       };
     });
