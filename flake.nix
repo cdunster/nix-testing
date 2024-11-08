df0623b10ecb356b285bc0aba235236433fd7696
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-08 13:32:14 +0100

    chore: use flake-utils for devShells

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "flake-utils";
+  };
 
-  outputs = { ... }@inputs:
-    let
+  outputs = { ... }@inputs: {
+    nixosConfigurations.test-vm = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      modules = [
+        ./nixos/configuration.nix
+      ];
+    };
+  } // inputs.flake-utils.lib.eachDefaultSystem (system:
+    let
       pkgs = inputs.nixpkgs.legacyPackages.${system};
     in
     {
-      nixosConfigurations.test-vm = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./nixos/configuration.nix
-        ];
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.default = pkgs.mkShell {
         QEMU_NET_OPTS = "hostfwd=tcp::2221-:22"; # Forward VM SSH port to host port 2221
       };
-    };
+    });
 }
